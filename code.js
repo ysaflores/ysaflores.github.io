80a9b7426813f942f6bd8ca4c4b5e7db708f4a09
Ysabelle Flores <yflores14@smpanthers.org>
2014-05-12 14:06:40 -0700

    code for javascript for index.html page

---
+++ code.js
@@ -0,0 +1,10 @@
+function button1() {
+	alert("you are the best :)");
+        $("#buttonone").html("until next time!");
+        $("#buttonone").fadeOut();
+}
+
+function button2() {
+	$("#buttontwo").html("and until we meet again...");
+        $("#buttontwo").fadeOut();
+}
