4a9a65f0bf88550b9cc58be6e03e03b2b51983c0
Ysabelle Flores <yflores14@smpanthers.org>
2014-05-15 12:11:51 -0700

    not finished

--- collatz.js
+++ collatz.js
@@ -1,1 +1,23 @@
 console.log("Ysabelle's Collatz");
+    var x = 59
+    function collatz(x) {
+    length = 0
+    console.log(x)
+}
+
+    while(x>1){
+        if(x%2==0) {
+            x=x/2;
+     }  else {
+            x = 3*x + 1;
+            console.log(x)
+            length++;
+}
+
+    return length;
+
+start = 59
+length = collatz(start)
+collatz("\ncollatz(%d) has %d items" % (start, length))
+
+
