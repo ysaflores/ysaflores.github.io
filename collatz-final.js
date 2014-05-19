e75c0a9252d9a9108cbdf441105451705846247c
Ysabelle Flores <yflores14@smpanthers.org>
2014-05-19 13:54:07 -0700

    modified changes for collatz.js

--- collatz-final.js
+++ collatz-final.js
@@ -1,24 +1,21 @@
 console.log("Ysabelle's Collatz");
-   function collatz(x) {
-   var x = 59;
-   length = 0;
+   var x = 59
+   var length = 0
    console.log(x);
-}
 
    while(x>1){
        if(x%2==0) {
-           x=x/2
-    }  
-else {
-           3*x+1
-    }
+           x=x/2;
+       }  
+       else {
+          x= 3*x+1}
 
    console.log(x);
    length++
-
-   return length
+       }
+   return length;
 
 var start = 59
-length = collatz(start)
-console.log("\ncollatz(%d) has %d items" % (start, length))
+var length = collatz(start)
+console.log("\n collatz(%d) has %d items" % (start, length))
 
